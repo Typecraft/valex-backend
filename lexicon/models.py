136438d0e3cb20bd66d264d089aa2bfd16a3f81e
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-07 14:57:08 +0200

    Add basic models

--- lexicon/models.py
+++ lexicon/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lemma(models.Model):
+    LANGUAGE_CHOICES = (
+        ('nob', 'Norwegian'),
+        ('deu', 'German')
+    )
+    lemma = models.CharField(max_length=255)
+    language = models.CharField(max_length=4, choices=LANGUAGE_CHOICES)
+
+
+class Meaning(models.Model):
+    meaning = models.TextField()
+    lemma = models.ForeignKey(Lemma, on_delete=models.CASCADE)
+    ontologyToken = models.CharField(max_length=4)
+
+
+class ValenceFrame(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+
+class MeaningValence(models.Model):
+    meaning = models.ForeignKey(Meaning, on_delete=models.CASCADE, related_name='valences')
+    valenceFrame = models.ForeignKey(ValenceFrame, on_delete=models.CASCADE, related_name='examples')
+
+
+class Example(models.Model):
+    meaningValence = models.ForeignKey(MeaningValence, on_delete=models.CASCADE, related_name='examples')
+    text = models.TextField()
+    link = models.TextField()
