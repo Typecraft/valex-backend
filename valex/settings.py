ae086f8218d5532d51c7d90eedb6bc4838abced2
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-09 09:07:10 +0200

    Add import prod settings

--- valex/settings.py
+++ valex/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if 'PROD' in os.environ:
+    try:
+        from settings_prod import *
+    except ImportError:
+        pass
