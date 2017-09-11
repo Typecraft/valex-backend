7c0513f79fdf013dbb3ab068a3bfa7c6541a5acb
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-11 16:56:22 +0200

    Add default redirect to /app/

--- valex/urls.py
+++ valex/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
     url(r'^app/', include('app.urls')),
     url(r'^', include('users.urls')),
+    url(r'^$', RedirectView.as_view(url='/app/', permanent=False))
 ]
