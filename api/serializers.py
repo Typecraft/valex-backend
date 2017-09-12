6317b8f6aecc94766ca4f0f12e7a95322e677e30
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-12 06:21:51 +0200

    Update UserSerializer add is_staff etc. fields

--- api/serializers.py
+++ api/serializers.py
@@ -7,8 +7,8 @@ from lexicon.models import Lemma, Meaning, ValenceFrame, MeaningValence, Example
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'email')
-        read_only_fields = ('url', 'id')
+        fields = ('url', 'id', 'username', 'email', 'is_active', 'is_staff', 'is_superuser')
+        read_only_fields = ('url', 'id', 'is_active', 'is_staff', 'is_superuser')
 
 
 class LemmaSerializer(serializers.ModelSerializer):
