005a15e4cc1a2d04894d2c96bbd0d4f64b556d41
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-13 18:44:34 +0200

    Add first_name and last_name to serialization

--- api/serializers.py
+++ api/serializers.py
@@ -7,14 +7,14 @@ from lexicon.models import Lemma, Meaning, ValenceFrame, MeaningValence, Example
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'email', 'is_active', 'is_staff', 'is_superuser')
+        fields = ('url', 'id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
         read_only_fields = ('url', 'id', 'is_active', 'is_staff', 'is_superuser')
 
 
 class LemmaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lemma
-        fields = ('url', 'id', 'lemma', 'meanings',)
+        fields = ('url', 'id', 'lemma', 'language', 'meanings',)
 
 
 class MeaningSerializer(serializers.ModelSerializer):
