c8d6184978358d113b422b0079e855aa566477b0
Tormod Haugland <tormod.haugland@gmail.com>
2017-10-23 11:08:57 +0200

    Add lemma mock command

---
+++ lexicon/management/commands/mocklemmas.py
@@ -0,0 +1,19 @@
+import json
+import os
+from django.core.management import BaseCommand
+
+from lexicon.models import Lemma
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open(os.path.join(os.path.dirname(__file__), 'lemmas.json')) as f:
+            lemmas = json.load(f)
+            Lemma.objects.bulk_create([
+                Lemma(
+                    lemma=lemma,
+                    language='deu'
+                ) for lemma in lemmas
+            ])
+
+
